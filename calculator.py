9956320d5d9ce764091902599c0ddff5f878b61e
Anjum <haj.huzaifa@gmail.com>
2021-06-17 12:19:30 +0530

    Added button 0 and button 1

--- calculator.py
+++ calculator.py
@@ -23,3 +23,15 @@ class Application(Frame):
         self.user_input.grid(columnspan=4)
 
         self.user_input.insert(0, "0")
+
+        # Button for value 0
+        self.button9 = Button(self, bg="#98DBC6", bd=12,
+                              text="0",  padx=33, pady=25,
+                              command=lambda: self.buttonClick(0), font=("Helvetica", 20, "bold"))
+        self.button9.grid(row=5, column=0, sticky=W)
+
+        # Button for value 1
+        self.button7 = Button(self, bg="#98DBC6", bd=12,
+                              text="1",  padx=33, pady=25,
+                              command=lambda: self.buttonClick(1), font=("Helvetica", 20, "bold"))
+        self.button7.grid(row=4, column=0, sticky=W)
